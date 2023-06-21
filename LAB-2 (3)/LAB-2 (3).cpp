b8533b888a1f6356ef5c13bf884341c695310a5f
Шуманович Егор <78348960+Camper02@users.noreply.github.com>
2023-06-21 12:41:20 +0200

    Add files via upload

---
+++ LAB-2 (3)/LAB-2 (3).cpp
@@ -0,0 +1,30 @@
+﻿#include "LAB-2 (3).h"
+
+using namespace std;
+
+int main()
+{
+    Complex a(2, 3);
+    Complex b(4, 5);
+    cout << a << endl;
+    cout << b << endl;
+
+    Complex c = a + b;
+    cout << c << endl;
+    cout << a * b << endl;
+
+    a += b;
+    cout << a << endl;
+    a *= b;
+    cout << a << endl;
+
+    cout << ++a << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+
+    Complex e;
+    cin >> e;
+    cout << e << endl;
+
+    return 0;
+}

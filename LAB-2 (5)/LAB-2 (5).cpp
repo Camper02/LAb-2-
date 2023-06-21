60645578b69ad7edb447e1136a91fd39a0db7744
Шуманович Егор <78348960+Camper02@users.noreply.github.com>
2023-06-21 14:01:16 +0200

    Add files via upload

---
+++ LAB-2 (5)/LAB-2 (5).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "LAB-2 (5).h"
+
+using namespace std;
+
+int main() {
+    Vector<bool> vec;
+    vec.push_back(1);
+    vec.push_back(0);
+    vec.push_back(0);
+    vec.push_back(1);
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec[2] << endl;
+    cout << vec[3] << endl;
+    cout << vec.size() << endl;
+    vec.erase(1, 2);
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec.size() << endl;
+    vec.insert(0, 1);
+    cout << vec[1] << endl;
+    vec[1] = 1;
+    cout << vec[1] << endl;
+    cout << vec.size() << endl;
+    return 0;
+}
---
+++ LAB-2 (5)/LAB-2 (5).h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+
+template<typename VCTR>
+class Vector
+{
+private:
+    VCTR* v;
+    int s = 0;
+public:
+    Vector()
+    {
+        VCTR* v = new VCTR[s];
+    }
+    ~Vector()
+    {
+        delete[] v;
+    }
+    VCTR push_back(bool inp)
+    {
+        return v[s++] = inp;
+    }
+    bool& operator[](int ind)
+    {
+        return v[ind];
+    }
+    int size()
+    {
+        return s;
+    }
+    VCTR insert(bool inp, int ind)
+    {
+        int k = s;
+        VCTR* v1 = v;
+        while (s > 0)
+        {
+            v[s--];
+        }
+        for (int i = 0; i < k - 1; i++)
+        {
+            if (i == ind)
+            {
+                v[s++] = inp;
+            }
+            v[s++] = v1[i];
+
+        }
+        return v;
+    }
+    VCTR erase(int s_i, int c = 1)
+    {
+        int k = s + 1;
+        VCTR* v1 = v;
+        while (s > 0)
+        {
+            v[s--];
+        }
+        for (int i = 0; i < k; i++)
+        {
+            if (i < s_i or i > s_i + c - 1)
+            {
+                v[s++] = v1[i];
+            }
+        }
+        return v;
+    }
+};
+
